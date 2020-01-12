9876b5ada62dff7a2aab57544010f908aedd42d3
Neil Nathan <info@neilnathan.io>
2020-01-12 09:55:54 +0100

    Updates

---
+++ precache-manifest.00fed0cec78f329c1a05d8c4799f417a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c60d9c6eb5046b7ff27b1725afddd56",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "b0f79c196734486c8763",
+    "url": "/monsters-rolodex2/static/css/main.d8b283b9.chunk.css"
+  },
+  {
+    "revision": "0efc1066f364d5ff6b5a",
+    "url": "/monsters-rolodex2/static/js/2.f19e69e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex2/static/js/2.f19e69e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0f79c196734486c8763",
+    "url": "/monsters-rolodex2/static/js/main.0253b011.chunk.js"
+  },
+  {
+    "revision": "182ae11938dd949d3bdd",
+    "url": "/monsters-rolodex2/static/js/runtime-main.df301dfe.js"
+  }
+]);
